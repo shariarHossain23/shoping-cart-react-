02d016eca1d053a70383f0f154cf6fe48a022f0a
shariarhossain23e <shariarhossain23@gamil.comm>
2022-03-19 12:52:13 +0600

    data loaded

--- src/component/Allproduct/Allproduct.js
+++ src/component/Allproduct/Allproduct.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Allproduct = () => {
     const [products,setProducts] = useState([])
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=> setProducts(json))
+    },[])
     return (
         <div>
-            
+            <div>
+                {products.map(pd => console.log(pd))}
+            </div>
         </div>
     );
 };
