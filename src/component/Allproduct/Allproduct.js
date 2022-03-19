2555b42ca07954a1fa2b93595f726718b82dacb0
shariarhossain23e <shariarhossain23@gamil.comm>
2022-03-19 12:45:56 +0600

    setup

---
+++ src/component/Allproduct/Allproduct.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Allproduct = () => {
+    const [products,setProducts] = useState([])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Allproduct;
